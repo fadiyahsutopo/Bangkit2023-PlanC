9e2a4b2cfe1293f636deeb9aeb39356713b485aa
fadiyahsutopo <fadiyah.sutopo@gmail.com>
2023-06-11 19:33:11 +0700

    home

--- src/ui/component/Categories/Category.jsx
+++ src/ui/component/Categories/Category.jsx
@@ -1,23 +1,26 @@
 import { Box, Image, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export function Category() {
   return (
     <Box width="200px" 
-    height="200px" 
-    borderRadius="27px" 
-    overflow="hidden" 
-    backgroundImage={require('../../../assets/images/beautiful.jpg')}
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    backgroundPosition="center"
-    backgroundRepeat="no-repeat">
-      
-        <Text fontWeight="bold" fontSize="25pt" mb="2" color="white">
+      height="200px" 
+      borderRadius="27px" 
+      overflow="hidden" 
+      backgroundImage={require('../../../assets/images/beautiful.jpg')}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat">
+      <Link to="/list">
+      <Text fontWeight="bold" fontSize="25pt" mb="2" color="white">
           Pantai
         </Text>
+        
+      </Link>
+        
       
-    </Box>
-  );
+    </Box>
+  );
 }
-
