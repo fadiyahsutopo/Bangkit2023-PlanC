f5b9fb99cbc479c23f4fd8d9c2c8f0fe90913836
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2023-06-12 21:23:04 +0700

    feat: add link to categories

--- src/ui/pages/Categories/Categories.jsx
+++ src/ui/pages/Categories/Categories.jsx
@@ -1,22 +1,25 @@
 import { Box, HStack, SimpleGrid, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export function Categories() {
   return (
     <SimpleGrid columns={2} spacing={10} w="full" pt={20} pl={20}>
-      <HStack gap={10}>
-        <Box
-          width="200px"
-          height="200px"
-          borderRadius="27px"
-          overflow="hidden"
-          backgroundImage={require("../../../assets/images/beautiful.jpg")}
-          backgroundPosition="center"
-          backgroundRepeat="no-repeat"
-        ></Box>
-        <Text fontWeight="bold" fontSize="25pt" mb="2">
-          Pantai
-        </Text>
-      </HStack>
+      <Link to="/place">
+        <HStack gap={10}>
+          <Box
+            width="200px"
+            height="200px"
+            borderRadius="27px"
+            overflow="hidden"
+            backgroundImage={require("../../../assets/images/beautiful.jpg")}
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+          ></Box>
+          <Text fontWeight="bold" fontSize="25pt" mb="2">
+            Pantai
+          </Text>
+        </HStack>
+      </Link>
       <HStack gap={10}>
         <Box
           width="200px"
