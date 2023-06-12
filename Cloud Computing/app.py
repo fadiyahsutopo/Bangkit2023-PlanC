cd5f2e6ff96b39e8c30b30b6e4e0221000d2af00
mahdi imani wafi <73066863+mahdiwafi@users.noreply.github.com>
2023-06-13 05:44:57 +0700

    Handle BE Endpoints

--- Cloud Computing/app.py
+++ Cloud Computing/app.py
@@ -6,6 +6,7 @@ from keras.utils.data_utils import get_file
 
 import pandas as pd
 import numpy as np
+import math
 
 app = Flask(__name__)
 CORS(app)
@@ -28,7 +29,7 @@ def recommend_id(user_id=12, num_of_rec=7):
     keras_model = load_model(model_path)
     rating = pd.read_csv(
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/user_rating.csv')
-    destination = pd.read_csv(
+    destinations = pd.read_csv(
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
     destination_array = np.array(list(set(rating.place_id)))
     user = np.array([user_id for i in range(len(destination_array))])
@@ -37,13 +38,46 @@ def recommend_id(user_id=12, num_of_rec=7):
     predictions = np.array([a[0] for a in predictions])
     recommended_place_id = (-predictions).argsort()[:num_of_rec]
 
-    recommendation = destination[destination['place_id'].isin(
-        recommended_place_id)].to_json(orient='records')
+    recommendation = pd.DataFrame()
+    for i in recommended_place_id:
+        recommendation = pd.concat(
+            [recommendation, destinations[destinations['place_id'] == i]], ignore_index=True)
+        recommendation.reset_index()
+    recommendation = recommendation.to_json(orient='records')
 
-    # print(num_of_rec, 'recommendations for', user_id)
-    # print(recommended_place_id)
-    # print(predictions[recommended_place_id])
     return recommendation
+
+@app.route('/nearest')
+@app.route('/nearest/<int:lat>/<int:lng>')
+@app.route('/nearest/<int:lat>/<int:lng>/<int:num>')
+def nearest(lat=0.0, lng=0.0, num=5):
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    destinations = destinations.reset_index()
+    nearest_dist = [math.inf] * num
+    nearest_id = [0] * num
+    for index, n in destinations.iterrows():
+        dist = math.dist([lat, lng], [n['lat'], n['lng']])
+        for i in range(num):
+            if nearest_dist[i] > dist:
+                prev_dist = nearest_dist[i]
+                prev_id = nearest_id[i]
+                nearest_dist[i] = dist
+                nearest_id[i] = n['place_id']
+                for j in range(1, num-i):
+                    prev_dist = nearest_dist[i+j]
+                    prev_id = nearest_id[i+j]
+                    if(i+j != num):
+                        nearest_dist[i+j] = prev_dist
+                        nearest_id[i+j] = prev_id
+                break
+    nearest = pd.DataFrame()
+    for i in nearest_id:
+        nearest = pd.concat(
+            [nearest, destinations[destinations['place_id'] == i]], ignore_index=True)
+        nearest.reset_index()
+    nearest = nearest.to_json(orient='records')
+    return nearest
 
 @app.route('/destination/<int:dest_id>')
 def destination(dest_id):
@@ -51,6 +85,26 @@ def destination(dest_id):
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
     destination = destinations[destinations['place_id']
                                == dest_id].to_json(orient='records')
+    return destination
+
+
+@app.route('/categories')
+def categories():
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    types = destinations['types'].unique()
+    categories = {}
+    for type in types:
+        categories.update(
+            {type: destinations[destinations['types'] == type]['photos'].sample().iloc[0]})
+    return categories
+
+@app.route('/category/<string:category>')
+def category(category):
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    destination = destinations[destinations['types']
+                               == category].to_json(orient='records')
     return destination
 
 @app.route('/user/<int:user_id>')
--- Cloud Computing/main.py
+++ Cloud Computing/main.py
@@ -7,6 +7,7 @@ import requests
 import urllib.parse
 import json
 from flask import Flask, url_for, request
+import math
 
 from keras.models import load_model, model_from_json
 import tensorflow as tf
@@ -15,6 +16,7 @@ import tensorflow_recommenders as tfrs
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras.utils.data_utils import get_file
+import json
 # import tensorflow_hub as hub
 
 # from keras.model import load_weights
@@ -112,9 +114,16 @@ app = Flask(__name__)
 def classification():
     return 'API working'
 
+
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/api')
+def api():
+    return "API Running"
+
 
 @app.route('/recommend')
 @app.route('/recommend/<int:user_id>')
@@ -126,7 +135,7 @@ def recommend_id(user_id=12, num_of_rec=7):
     keras_model = load_model(model_path)
     rating = pd.read_csv(
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/user_rating.csv')
-    destination = pd.read_csv(
+    destinations = pd.read_csv(
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
     destination_array = np.array(list(set(rating.place_id)))
     user = np.array([user_id for i in range(len(destination_array))])
@@ -135,13 +144,59 @@ def recommend_id(user_id=12, num_of_rec=7):
     predictions = np.array([a[0] for a in predictions])
     recommended_place_id = (-predictions).argsort()[:num_of_rec]
 
-    recommendation = destination[destination['place_id'].isin(
-        recommended_place_id)].to_json(orient='records')
+    # recommendation = destinations[destinations['place_id'].isin(
+    #     recommended_place_id)].to_json(orient='records')
+
+    recommendation = pd.DataFrame()
+    for i in recommended_place_id:
+        recommendation = pd.concat(
+            [recommendation, destinations[destinations['place_id'] == i]], ignore_index=True)
+        recommendation.reset_index()
+    recommendation = recommendation.to_json(orient='records')
 
     # print(num_of_rec, 'recommendations for', user_id)
     # print(recommended_place_id)
     # print(predictions[recommended_place_id])
     return recommendation
+
+
+@app.route('/nearest')
+@app.route('/nearest/<int:lat>/<int:lng>')
+@app.route('/nearest/<int:lat>/<int:lng>/<int:num>')
+def nearest(lat=0.0, lng=0.0, num=5):
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    destinations = destinations.reset_index()
+    nearest_dist = [math.inf] * num
+    nearest_id = [0] * num
+    for index, n in destinations.iterrows():
+        # print(index)
+        # print(nearest_id)
+        # print(nearest_dist)
+        dist = math.dist([lat, lng], [n['lat'], n['lng']])
+        for i in range(num):
+            if nearest_dist[i] > dist:
+                prev_dist = nearest_dist[i]
+                prev_id = nearest_id[i]
+                nearest_dist[i] = dist
+                nearest_id[i] = n['place_id']
+                for j in range(1, num-i):
+                    prev_dist = nearest_dist[i+j]
+                    prev_id = nearest_id[i+j]
+                    if(i+j != num):
+                        nearest_dist[i+j] = prev_dist
+                        nearest_id[i+j] = prev_id
+                break
+
+    nearest = pd.DataFrame()
+    for i in nearest_id:
+        nearest = pd.concat(
+            [nearest, destinations[destinations['place_id'] == i]], ignore_index=True)
+        nearest.reset_index()
+    nearest = nearest.to_json(orient='records')
+    # nearest = destinations[destinations['place_id'].isin(
+    #     nearest_id)].to_json(orient='records')
+    return nearest
 
 
 @app.route('/destination/<int:dest_id>')
@@ -150,6 +205,27 @@ def destination(dest_id):
         'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
     destination = destinations[destinations['place_id']
                                == dest_id].to_json(orient='records')
+    return destination
+
+
+@app.route('/categories')
+def categories():
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    types = destinations['types'].unique()
+    categories = {}
+    for type in types:
+        categories.update(
+            {type: destinations[destinations['types'] == type]['photos'].sample().iloc[0]})
+    return categories
+
+
+@app.route('/category/<string:category>')
+def category(category):
+    destinations = pd.read_csv(
+        'https://storage.googleapis.com/planc-product-capstone-bucket/keras/planc_destinations.csv')
+    destination = destinations[destinations['types']
+                               == category].to_json(orient='records')
     return destination
 
 
